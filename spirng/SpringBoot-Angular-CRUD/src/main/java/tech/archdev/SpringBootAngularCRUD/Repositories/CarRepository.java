8a25e0f5891815926a80197b61a6e44d9f1dda88
SBeard <etacalpha@gmail.com>
2018-11-20 22:44:07 -0500

    started front end for springBoot Angulr CRUD

--- spirng/SpringBoot-Angular-CRUD/src/main/java/tech/archdev/SpringBootAngularCRUD/Repositories/CarRepository.java
+++ spirng/SpringBoot-Angular-CRUD/src/main/java/tech/archdev/SpringBootAngularCRUD/Repositories/CarRepository.java
@@ -2,8 +2,10 @@ package tech.archdev.SpringBootAngularCRUD.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import tech.archdev.SpringBootAngularCRUD.Domains.Car;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
--- spirng/SpringBoot-Angular-CRUD/src/main/java/tech/archdev/SpringBootAngularCRUD/Rest/CoolCarResource.java
+++ spirng/SpringBoot-Angular-CRUD/src/main/java/tech/archdev/SpringBootAngularCRUD/Rest/CoolCarResource.java
@@ -1,5 +1,6 @@
 package tech.archdev.SpringBootAngularCRUD.Rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.archdev.SpringBootAngularCRUD.Domains.Car;
@@ -17,6 +18,7 @@ public class CoolCarResource {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return service.getAllCars().stream()
                 .filter(this::isCool)
