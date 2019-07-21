f3cef864e8bdfa1266f22d33f50c2cf5ad0c962f
etacalpha <sbeard@intellicog.com>
2019-07-21 15:21:50 -0400

    added profile app

--- AWS/profileApp/src/app/app.module.ts
+++ AWS/profileApp/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AuthComponent } from "./auth/auth.component";
+import { ProfileComponent } from "./profile/profile.component";
+
+import { AmplifyAngularModule, AmplifyService } from "aws-amplify-angular";
+
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AuthComponent, ProfileComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAngularModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule
   ],
-  providers: [],
+  providers: [AmplifyService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
