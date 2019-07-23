f7740b97545c2d5747735568426ae80a4fa41e13
etacalpha <sbeard@intellicog.com>
2019-07-23 10:25:22 -0400

    added react front end for blog

---
+++ react/blog/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticlePage from './pages/ArticlePage'
+import ArticleListPage from './pages/ArticleListPage'
+import Navbar from './Navbar';
+import NotFoundPage from './pages/NotFoundPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div id="page-body">
+          <Switch>
+            <Route path='/' component={HomePage} exact />
+            <Route path='/about' component={AboutPage} />
+            <Route path='/article-list' component={ArticleListPage} />
+            <Route path='/article/:name' component={ArticlePage} />
+
+            <Route component={NotFoundPage} />
+          </Switch>
+
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ react/blog/src/pages/AboutPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>About Me</h1>
+        <p>
+            Duis consequat deserunt tempor ullamco aliqua sit ut nulla Lorem proident ex.
+            Id est et nostrud consequat in elit aliquip in in elit sint. Sunt labore ad
+            sunt dolor consequat cillum.
+    </p>
+        <p>
+            Irure veniam ullamco id nisi cupidatat ipsum ipsum ex labore irure dolore elit
+            enim aute. Tempor duis officia excepteur Lorem duis. Irure cillum cillum aliquip
+            laboris commodo qui velit. Fugiat laborum nisi in velit enim. Culpa est amet irure
+            do nisi sit id cillum aliquip velit cupidatat velit ad velit. Sunt exercitation
+            ipsum ea id nulla aute proident ea veniam.
+    </p>
+        <p>
+            Deserunt labore mollit voluptate reprehenderit non aliquip esse. Eu nulla dolor
+            commodo laboris cillum in nostrud labore officia anim culpa proident in. Quis nulla
+            id exercitation incididunt ut veniam incididunt exercitation. Amet enim est non
+            anim ex.
+    </p>
+        <p>
+            Consequat nostrud et sint laborum nisi et. Deserunt quis adipisicing Lorem sit qui
+            adipisicing qui incididunt irure cillum ut labore reprehenderit. Culpa fugiat enim
+            in et duis consequat duis.
+    </p>
+    </>
+);
+
+export default AboutPage;
